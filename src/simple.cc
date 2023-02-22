dd04de70e6dab2a0ac5fd7b74e38054ab0965b38
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2023-02-22 17:47:41 +0100

    Initial commit

---
+++ src/simple.cc
@@ -0,0 +1,43 @@
+#include "glibmm/refptr.h"
+#include "gtkmm/applicationwindow.h"
+#include "gtkmm/box.h"
+#include "sigc++/functors/ptr_fun.h"
+#include <gtkmm/application.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/frame.h>
+#include <gtkmm/window.h>
+#include <iostream>
+
+class MainWindow : public Gtk::ApplicationWindow {
+public:
+  MainWindow(Glib::RefPtr<Gtk::Application> app);
+};
+
+MainWindow::MainWindow(Glib::RefPtr<Gtk::Application> app) : Gtk::ApplicationWindow(app) {
+  set_title("Mel Desktop");
+  set_default_size(200, 200);
+
+  //auto frame = Gtk::Frame();
+  //auto box = Gtk::Box();
+  //auto button1 = Gtk::Button();
+  //auto button2 = Gtk::Button();
+  //box.append(button1);
+  //box.append(button2);
+}
+
+void on_activated(Glib::RefPtr<Gtk::Application> app) {
+  std::cout << "activated" << std::endl;
+  auto window = new MainWindow(app);
+  app->add_window(*window);
+  window->present();
+}
+
+int main(int argc, char *argv[]) {
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+  app->signal_activate().connect(sigc::bind(sigc::ptr_fun(&on_activated), app));
+
+  //return app->make_window_and_run<MainWindow>(argc, argv);
+  return app->run(argc, argv);
+}
